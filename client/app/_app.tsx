61d839b0f800b0e4be8713666ec3bac9f00848ac
emad.panah <emad.panah@gmail.com>
2024-05-27 11:21:27 +0330

    remove files

--- client/app/_app.tsx
+++
@@ -1,8 +0,0 @@
-// src/pages/_app.js (or _app.tsx)
-import '../src/styles/style.css'; // Import Tailwind CSS styles
-import { AppProps } from 'next/app';
-
-export default function App({ Component, pageProps }:AppProps) {
-  return <Component {...pageProps} />;
-}
-
